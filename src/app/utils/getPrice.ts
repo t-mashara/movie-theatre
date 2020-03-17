6808c677af4739fed957e2edb2412706bdccb6f8
timur.mashara <timur.mashara@skywindgroup.com>
2020-03-17 21:22:41 +0300

    doc comments

--- src/app/utils/getPrice.ts
+++ src/app/utils/getPrice.ts
@@ -1,8 +1,17 @@
 import {CartItem} from '../core/model/CartItem';
 
+/**
+ * Returns a price for a promo product
+ */
 const getPricePromo = ({ quantity, price, promo: { requiredAmount, priceAmount } }: CartItem) =>
   ((quantity % requiredAmount) + Math.floor(quantity / requiredAmount ) * priceAmount) * price;
 
+/**
+ * Returns a price for regular product
+ */
 const getPriceDefault = ({ price, quantity}: CartItem) => price * quantity;
 
+/**
+ * Returns a price for a cartItem (product)
+ */
 export const getPrice = (cartItem: CartItem) => cartItem.promo ? getPricePromo(cartItem) : getPriceDefault(cartItem);
